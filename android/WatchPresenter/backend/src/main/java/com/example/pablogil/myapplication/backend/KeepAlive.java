43137d3d1bd78624104d3eb4d44256bfc70bf37e
Pablo Gil Montaño <pablogil@google.com>
2015-05-07 00:45:53 +0200

    Keep alive on the server

---
+++ android/WatchPresenter/backend/src/main/java/com/example/pablogil/myapplication/backend/KeepAlive.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Backend with Google Cloud Messaging" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/GcmEndpoints
+*/
+
+package com.example.pablogil.myapplication.backend;
+
+import com.google.appengine.api.oauth.OAuthRequestException;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static com.example.pablogil.myapplication.backend.OfyService.ofy;
+
+
+/**
+ * Simple Servlet to invoke to keep the application alive
+ */
+public class KeepAlive extends HttpServlet{
+    private static final Logger log = Logger.getLogger(KeepAlive.class.getName());
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+            IOException {
+        log.fine("Keep alive received");
+    }
+
+}
