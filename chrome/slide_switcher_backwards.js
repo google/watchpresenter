6b466ba5b9654b479a3f5abcef0b206e663c5ff6
Pablo Gil Montaño <pablogil@google.com>
2015-05-24 18:14:28 +0200

    Going backwards by using mouse wheel

--- chrome/slide_switcher_backwards.js
+++ chrome/slide_switcher_backwards.js
@@ -12,4 +12,37 @@
 //See the License for the specific language governing permissions and
 //limitations under the License.
 
-window.history.go(-1);
+function eventFire(el, etype) {
+    if (el.fireEvent) {
+        (el.fireEvent('on' + etype));
+    } else {
+        var evObj = document.createEvent('Events');
+        evObj.initEvent(etype, true, false);
+        el.dispatchEvent(evObj);
+    }
+}
+
+
+function clickOnElement(el, doc) {
+    eventFire(el, "mouseover", doc);
+    eventFire(el, "focus", doc);
+    eventFire(el, "mousemove", doc);
+    eventFire(el, "mousedown", doc);
+    eventFire(el, "mouseup", doc);
+    var evt = document.createEvent("Events");
+    evt.initEvent('mousewheel', true, false);
+    evt.wheelDelta = -120;
+    el.dispatchEvent(evt);
+}
+
+var elementToClickOn = document.evaluate("//*[@class=\"punch-viewer-container\"]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+var documentToClickOn = document;
+if (elementToClickOn) {
+
+} else {
+    documentToClickOn = document.evaluate("//*[@class=\"punch-present-iframe\"]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.contentDocument;
+
+    elementToClickOn = documentToClickOn.evaluate("//*[@class=\"punch-viewer-container\"]", documentToClickOn, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+clickOnElement(elementToClickOn, documentToClickOn);
