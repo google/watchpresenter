c63a4b1497f908ed21a1ec9c9a29c62037aabb22
Pablo Gil Montaño <pablogil@pablogil-macbookpro.roam.corp.google.com>
2017-09-23 12:10:48 +0200

    Fixed going backwards

--- chrome/slide_switcher.js
+++ chrome/slide_switcher.js
@@ -12,34 +12,16 @@
 //See the License for the specific language governing permissions and
 //limitations under the License.
 
-function eventFire(el, etype) {
-    if (el.fireEvent) {
-        (el.fireEvent('on' + etype));
-    } else {
-        var evObj = document.createEvent('Events');
-        evObj.initEvent(etype, true, false);
-        el.dispatchEvent(evObj);
-    }
-}
-
-
-function clickOnElement(el, doc) {
-    eventFire(el, "mouseover", doc);
-    eventFire(el, "focus", doc);
-    eventFire(el, "mousemove", doc);
-    eventFire(el, "mousedown", doc);
-    eventFire(el, "mouseup", doc);
-    eventFire(el, "click", doc);
-}
-
 var elementToClickOn = document.evaluate("//*[@class=\"punch-viewer-container\"]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-var documentToClickOn = document;
 if (elementToClickOn) {
 
 } else {
-    documentToClickOn = document.evaluate("//*[@class=\"punch-present-iframe\"]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.contentDocument;
+    var documentToClickOn = document.evaluate("//*[@class=\"punch-present-iframe\"]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.contentDocument;
 
     elementToClickOn = documentToClickOn.evaluate("//*[@class=\"punch-viewer-container\"]", documentToClickOn, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
-clickOnElement(elementToClickOn, documentToClickOn);
+var evt = new WheelEvent("wheel", {
+    deltaY: 1
+  });
+elementToClickOn.dispatchEvent(evt);
--- chrome/slide_switcher_backwards.js
+++ chrome/slide_switcher_backwards.js
@@ -12,37 +12,18 @@
 //See the License for the specific language governing permissions and
 //limitations under the License.
 
-function eventFire(el, etype) {
-    if (el.fireEvent) {
-        (el.fireEvent('on' + etype));
-    } else {
-        var evObj = document.createEvent('Events');
-        evObj.initEvent(etype, true, false);
-        el.dispatchEvent(evObj);
-    }
-}
-
-
-function clickOnElement(el, doc) {
-    eventFire(el, "mouseover", doc);
-    eventFire(el, "focus", doc);
-    eventFire(el, "mousemove", doc);
-    eventFire(el, "mousedown", doc);
-    eventFire(el, "mouseup", doc);
-    var evt = document.createEvent("Events");
-    evt.initEvent('mousewheel', true, false);
-    evt.wheelDelta = -120;
-    el.dispatchEvent(evt);
-}
-
 var elementToClickOn = document.evaluate("//*[@class=\"punch-viewer-container\"]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-var documentToClickOn = document;
 if (elementToClickOn) {
 
 } else {
-    documentToClickOn = document.evaluate("//*[@class=\"punch-present-iframe\"]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.contentDocument;
+    console.log("Looking for iframe...");
+    var documentToClickOn = document.evaluate("//*[@class=\"punch-present-iframe\"]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.contentDocument;
 
     elementToClickOn = documentToClickOn.evaluate("//*[@class=\"punch-viewer-container\"]", documentToClickOn, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
-clickOnElement(elementToClickOn, documentToClickOn);
+var evt = new WheelEvent("wheel", {
+    deltaY: -1
+  });
+elementToClickOn.dispatchEvent(evt);
+
